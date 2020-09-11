63a742b9c529e81a402298ec52e09ad9e42d578d
wojciechmaciejewski <wojciechmaciejewski1985@gmail.com>
2020-09-11 12:58:14 +0200

    add some place for movie details

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 
 API key from https://developers.themoviedb.org/ should be put in local.properties like this
-api_key = "01ad34fe36263da23ac21ab606c06745"
+api_key = "here_put_your_key"
---
+++ details/src/main/java/pl/wojtek/details/ui/MovieDetailsFragment.kt
@@ -0,0 +1,11 @@
+package pl.wojtek.details.ui
+
+import androidx.fragment.app.Fragment
+import pl.wojtek.details.R
+
+/**
+ *
+ */
+
+
+class MovieDetailsFragment : Fragment(R.layout.fragment_movie_details)
